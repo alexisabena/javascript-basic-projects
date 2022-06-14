78d6e9694c112dcae2f4d160e75a86572a83e888
alexisabena <32970506+alexisabena@users.noreply.github.com>
2022-06-13 23:38:33 -0500

    adding html and basic js

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,10 @@
+//Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+console.log(btns);
+
+btns
